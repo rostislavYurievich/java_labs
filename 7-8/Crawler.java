3d9f0b4c9566ac0a443facf8f710d2c96064af1e
Rostislav Yurievich <rostislav@yurievich.ru>
2023-03-28 14:47:13 +0300

    Work on lab 7 started

--- 7-8/Crawler.java
+++ 7-8/Crawler.java
@@ -1,12 +1,36 @@
 import java.util.LinkedList;
+import java.net.URL;
+import java.net.URLConnection;
+import java.nio.channels;
 
 class Crawler {
-    public static final string URL_PREFIX = "http: //"
+    public static final string URL_PREFIX = "http://"
     LinkedList<URLDepthPair> visitedUrls = new LinkedList<URLDepthPair>;
     LinkedList<URLDepthPair> unvisitedUrls = new LinkedList<URLDepthPair>;
 
     public LinkedList<URLDepthPair> getSites()
     {
         return visitedUrls;
+    }
+
+    public download(String url){
+        URL site = new URL("http",url.toString,80);
+        ReadableByteChannel rbc = Channels.newChannel(site.openStream());
+        File file = new File("./download"+URL.getPath());
+        file.getParentFile().mkdirs();
+        FileOutputStream fos = new FileOutputStream(file);
+        try {
+            fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
+            }
+        catch (Exception x){
+            System.println("File IO error!");
+        }
+        
+    }
+
+    public parse(String path){
+        File file = new File(path);
+        
+
     }
 }
