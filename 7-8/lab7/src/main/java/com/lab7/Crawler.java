1f2bfb0db4efdc7663ca143f2b632e4df3c4a799
RostislavYurievich <rostislav@yurievich.ru>
2023-05-09 19:41:50 +0300

    Work on 7

--- 7-8/lab7/src/main/java/com/lab7/Crawler.java
+++ 7-8/lab7/src/main/java/com/lab7/Crawler.java
@@ -2,14 +2,19 @@ package com.lab7;
 
 import java.util.LinkedList;
 import java.util.Scanner;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.net.MalformedURLException;
 import java.net.URL;
-import java.net.URLConnection;
 import java.nio.channels.Channels;
 import java.nio.channels.ReadableByteChannel;
+
 
 
 class Crawler {
@@ -44,37 +49,52 @@ class Crawler {
         unvisitedUrls.remove(udp);
     }
 
-    private boolean downloadURL(URLDepthPair udp){
-        return !visitedUrls.contains(udp);
+    private boolean isDownloaded(URLDepthPair udp){
+        return visitedUrls.contains(udp);
     }
 
-    private void parse(String path,int depth){
+    private void parse(String path,int depth) throws FileNotFoundException{
         File file = new File(path);
-        Scanner s = new Scanner(file);
-        s.useDelimiter("</a>");
-        while (s.hasNext()){
-            str = s.
+        Scanner scanner = new Scanner(file);
+        while(scanner.hasNextLine()) {
+            String s = scanner.nextLine();
+            Document doc = Jsoup.parse(s);
+            Element link = doc.select("a").first();
+            unvisitedUrls.add(new URLDepthPair(link.attr("href")));
         }
-        str.substring(str.indexOf("<a href =") + 1, str.indexOf(">"));
-
-        for (int i = 1; i<result.groupCount(); i++){
-            udp = new URLDepthPair(result.group(i),depth)
-            
-            unvisitedUrls.add();
-        }
-        s.close();
+        scanner.close();
     }
 
-    public void work(String startUrl,int depth) throws MalformedURLException{
+    public void work(String startUrl,int depth) throws IOException{
         unvisitedUrls.add(new URLDepthPair(startUrl,0));
-        for (URLDepthPair udp: unvisitedUrls){
-            if (udp.getDepth()<=depth){
+        while (!unvisitedUrls.isEmpty()){
+            URLDepthPair udp = unvisitedUrls.getFirst();
+            if ((udp.getDepth()<=depth)&&(!isDownloaded(udp))){
                 String url = udp.getUrl();
-                URL site = new URL(URL_PREFIX+url);
+                URL site = new URL(url);
                 File file = new File(downloadPath+site.getPath());
+                download(site,file);
                 parse(url,udp.getDepth()+1);
             }
             visitURL(udp);
+        }
+    }
+
+    public static void main(String[] args){
+        if (args.length != 2){
+            System.err.println("RTFM!");
+            System.exit(1);
+        }
+        try {
+            String downloadPath = "./download";
+            Crawler c = new Crawler(downloadPath);
+            c.work(args[0], Integer.parseInt(args[1]));
+        }
+        catch(Exception e){
+            System.err.println("Critical error!");
+            System.err.println(e.getMessage());
+            System.err.println(e.getStackTrace());
+            System.exit(1);
         }
     }
 }
